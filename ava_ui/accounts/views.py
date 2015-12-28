0ebcfc49fb79aa20a159ccfba2e055d5ed65c34c
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-12-28 19:28:58 +1300

    Changes for password-reset basics

--- ava_ui/accounts/views.py
+++ ava_ui/accounts/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 
-from ava_ui.abstract.utils import handle_error
+from ava_ui.abstract.utils import handle_error, csrf_post_request
 from ava_ui.accounts.models import UserToken
 
 log = logging.getLogger(__name__)
@@ -47,7 +47,6 @@ def login(request):
         return render(request, 'accounts/login.html', context=context)
 
 
-
 def authenticate(request, username, password):
     url = settings.API_BASE_URL + '/login/'
     login_data = {'username': username,
@@ -68,6 +67,7 @@ def logout(request):
     request.session['user'] = None
     return render(request, 'accounts/login.html')
 
+
 # class password_change():
 #
 #     template_name = 'accounts/password-change.html'
@@ -80,13 +80,48 @@ def logout(request):
 #     name = 'password_change_done'
 #
 #
-# class password_reset():
-#
-#     template_name = 'accounts/password-reset.html'
-#     name = 'password_reset'
-#
-#
-# class password_reset_done():
-#
-#     template_name = 'accounts/password-reset-done.html', 'post_reset_redirect=' / '
-#     name = 'password_reset_done'
+def password_reset(request):
+    template_name = 'accounts/password-reset.html'
+    url = settings.API_BASE_URL + '/password/reset/'
+    context = {'user': None}
+
+    if request.POST:
+        email = request.POST['email']
+        api_data = {'email': email}
+        results = csrf_post_request(url, data=api_data, headers={})
+
+        if results is not None:
+            if results.status_code == 200:
+                return redirect(settings.LOGIN_REDIRECT_URL, context=context)
+            else:
+                return handle_error(request, results.status_code)
+        else:
+            return HttpResponseRedirect('login')
+    else:
+        return render(request, template_name, context=context)
+
+
+def password_reset_confirm(request):
+    template_name = 'accounts/password-reset-confirm.html'
+    done_template_name = 'accounts/password-reset-done.html'
+    reset_template_name = 'accounts/password-reset.html'
+    url = settings.API_BASE_URL + '/accounts/password/reset/confirm/'
+    context = {'user': None}
+
+    if request.POST:
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        api_data = {'new_password1': password1,
+                    'new_password2': password2, }
+        results = csrf_post_request(url, data=api_data, headers={})
+
+        if results is not None:
+            if results.status_code == 200:
+                return render(request, done_template_name, context=context)
+            else:
+                return handle_error(request, results.status_code)
+        else:
+            # TODO is this the right template to send this to?
+            return render(request, reset_template_name, context=context)
+    else:
+        return render(request, template_name, context=context)
