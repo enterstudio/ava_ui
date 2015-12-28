6e4a8ea55ad0167759d6823db5aa80cf9b1c5a18
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-12-28 19:43:06 +1300

    Added helper to deal with the user context setting

--- ava_ui/abstract/utils.py
+++ ava_ui/abstract/utils.py
@@ -21,3 +21,12 @@ def csrf_post_request(request, url, api_data, headers):
 
     log.debug("csrf_post_request :: making POST request to " + str(url))
     return requests.post(url, data=api_data, headers=headers)
+
+
+def get_user_context(request):
+    context = {}
+    if request.session['user']:
+        context['user'] = request.session['user']
+    else:
+        context['user'] = None
+    return context
