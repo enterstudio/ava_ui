58ac9180eb15a430444380ab1b265e3629e0c88c
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-12-29 21:09:47 +1300

    Outline views and urls for my

--- ava_ui/my/urls.py
+++ ava_ui/my/urls.py
@@ -1,8 +1,51 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 from .views import *
 
 
 urlpatterns = [
-    # url(r'^', ''),
+    url(r'^profile/$', ProfileIndex.as_view(template_name=""),
+        name="my-profile-index"),
+
+    url(r'^profile/(?P<pk>[0-9]+)/$', ProfileDetail.as_view(template_name=""),
+        name="my-profile-detail"),
+
+    url(r'^profile/history/$', ActivityLogIndex.as_view(template_name=""),
+        name="my-profile-history-index"),
+
+    url(r'^profile/history/(?P<pk>[0-9]+)/$', ActivityLogDetail.as_view(template_name=""),
+        name="my-profile-history-detail"),
+
+    url(r'^friends/$', FriendsListIndex.as_view(template_name=""),
+        name="my-friends-index"),
+
+    url(r'^friends/(?P<pk>[0-9]+)/$', FriendsListDetail.as_view(template_name=""),
+        name="my-friends-detail"),
+    
+    url(r'^scorecard/$', ScoreCardIndex.as_view(template_name=""),
+        name="my-scorecard-index"),
+
+    url(r'^scorecard/(?P<pk>[0-9]+)/$', ScoreCardDetail.as_view(template_name=""),
+        name="my-scorecard-detail"),
+
+    url(r'^learning/history/$', LearningHistoryIndex.as_view(template_name=""),
+        name="my-learning-history-index"),
+
+    url(r'^learning/history/(?P<pk>[0-9]+)/$', LearningHistoryDetail.as_view(template_name=""),
+        name="my-learning-history-detail"),
+
+    url(r'^learning/queue/$', LearningQueueIndex.as_view(template_name=""),
+        name="my-learning-queue-index"),
+
+    url(r'^learning/queue/(?P<pk>[0-9]+)/$', LearningQueueDetail.as_view(template_name=""),
+        name="my-learning-queue-detail"),
+
+    url(r'^learning/profile/$', LearningProfileIndex.as_view(template_name=""),
+        name="my-learning-profile-index"),
+
+    url(r'^learning/profile/(?P<pk>[0-9]+)/$', LearningProfileDetail.as_view(template_name=""),
+        name="my-learning-profile-detail"),
+
+
+    
 ]
--- ava_ui/my/views.py
+++ ava_ui/my/views.py
@@ -1,7 +1,117 @@
 import logging
 
-import requests
-from django.shortcuts import render
-from django.views import generic
+from ava_ui.abstract.views import ObjectIndex, ObjectDetail
 
 log = logging.getLogger(__name__)
+
+
+class ProfileIndex(ObjectIndex):
+    url_suffix = '/my/profile/'
+    template_name = "my/profile/profile_index.html"
+
+    def get(self, request):
+        return super(ProfileIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class LearningHistoryIndex(ObjectIndex):
+    url_suffix = '/my/learning/history/'
+    template_name = "my/learning/history_index.html"
+
+    def get(self, request):
+        return super(LearningHistoryIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class LearningQueueIndex(ObjectIndex):
+    url_suffix = '/my/learning/queue/'
+    template_name = "my/learning/queue_index.html"
+
+    def get(self, request):
+        return super(LearningQueueIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class LearningProfileIndex(ObjectIndex):
+    url_suffix = '/my/learning/profile/'
+    template_name = "my/learning/profile_index.html"
+
+    def get(self, request):
+        return super(LearningProfileIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class FriendsListIndex(ObjectIndex):
+    url_suffix = '/my/friends/'
+    template_name = "my/friends/friends_index.html"
+
+    def get(self, request):
+        return super(FriendsListIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class ActivityLogIndex(ObjectIndex):
+    url_suffix = '/my/profile/history/'
+    template_name = "my/profile/history_index.html"
+
+    def get(self, request):
+        return super(ActivityLogIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class ScoreCardIndex(ObjectIndex):
+    url_suffix = '/my/scorecard/'
+    template_name = "my/scorecard/achievements_index.html"
+
+    def get(self, request):
+        return super(ScoreCardIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class ProfileDetail(ObjectDetail):
+    url_suffix = '/my/profile/'
+    template_name = "my/profile/profile_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(ProfileDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+
+
+class LearningHistoryDetail(ObjectDetail):
+    url_suffix = '/my/learning/history/'
+    template_name = "my/learning/history_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(LearningHistoryDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+
+
+class LearningQueueDetail(ObjectDetail):
+    url_suffix = '/my/learning/queue/'
+    template_name = "my/learning/queue_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(LearningQueueDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+
+
+class LearningProfileDetail(ObjectDetail):
+    url_suffix = '/my/learning/profile/'
+    template_name = "my/learning/profile_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(LearningProfileDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+
+
+class FriendsListDetail(ObjectDetail):
+    url_suffix = '/my/friends/'
+    template_name = "my/friends/friends_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(FriendsListDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+
+
+class ActivityLogDetail(ObjectDetail):
+    url_suffix = '/my/profile/history/'
+    template_name = "my/profile/history_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(ActivityLogDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+
+
+class ScoreCardDetail(ObjectDetail):
+    url_suffix = '/my/scorecard/'
+    template_name = "my/scorecard/achievements_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(ScoreCardDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
