5a5650806bf5995faaa5803f4172b4cc191d29e8
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-12-30 19:26:17 +1300

    Added template/email/slack views and urls (index and detail)

--- ava_ui/evaluate/urls.py
+++ ava_ui/evaluate/urls.py
@@ -1,8 +1,17 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 from .views import *
 
-
 urlpatterns = [
-    # url(r'^', ''),
+    url(r'^template/email/$', EvaluateTemplateEmailIndex.as_view(),
+        name="evaluate-template-email-index"),
+
+    url(r'^template/email/(?P<pk>[0-9]+)/$', EvaluateTemplateEmailDetail.as_view(),
+        name="evaluate-template-email-detail"),
+    
+    url(r'^template/slack/$', EvaluateTemplateSlackIndex.as_view(),
+        name="evaluate-template-slack-index"),
+
+    url(r'^template/slack/(?P<pk>[0-9]+)/$', EvaluateTemplateSlackDetail.as_view(),
+        name="evaluate-template-slack-detail"),
 ]
--- ava_ui/evaluate/views.py
+++ ava_ui/evaluate/views.py
@@ -5,3 +5,34 @@ from django.shortcuts import render
 from django.views import generic
 
 log = logging.getLogger(__name__)
+
+
+class EvaluateTemplateEmailIndex(ObjectIndex):
+    url_suffix = '/evaluate/template/email/'
+    template_name = "evaluate/template/email_template_index.html"
+
+    def get(self, request):
+        return super(EvaluateTemplateEmailIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class EvaluateTemplateEmailDetail(ObjectDetail):
+    url_suffix = '/evaluate/template/email/'
+    template_name = "evaluate/template/email_template_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(EvaluateTemplateEmailDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+    
+class EvaluateTemplateSlackIndex(ObjectIndex):
+    url_suffix = '/evaluate/template/slack/'
+    template_name = "evaluate/template/slack_template_index.html"
+
+    def get(self, request):
+        return super(EvaluateTemplateSlackIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class EvaluateTemplateSlackDetail(ObjectDetail):
+    url_suffix = '/evaluate/template/slack/'
+    template_name = "evaluate/template/slack_template_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(EvaluateTemplateSlackDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
