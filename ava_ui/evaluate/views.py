fe8145911e69f503b519c28cce7be50a00a33d03
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-12-31 20:09:14 +1300

    Updates to controller index and detail to match schema

--- ava_ui/evaluate/views.py
+++ ava_ui/evaluate/views.py
@@ -56,31 +56,31 @@ class EvaluateResultIndex(ObjectIndex):
 
 class EvaluateSenderIndex(ObjectIndex):
     url_suffix = '/evaluate/sender/'
-    sender_name = "evaluate/sender/sender_index.html"
+    template_name = "evaluate/sender/sender_index.html"
 
     def get(self, request):
-        return super(EvaluateSenderIndex, self).get(request, self.sender_name, self.url_suffix)
+        return super(EvaluateSenderIndex, self).get(request, self.template_name, self.url_suffix)
 
 
 class EvaluateSenderDetail(ObjectDetail):
     url_suffix = '/evaluate/sender/'
-    sender_name = "evaluate/sender/sender_detail.html"
+    template_name = "evaluate/sender/sender_detail.html"
 
     def get(self, request, **kwargs):
-        return super(EvaluateSenderDetail, self).get(request, self.sender_name, self.url_suffix, **kwargs)
+        return super(EvaluateSenderDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
 
 
 class EvaluateSenderCreate(ObjectCreate):
     url_suffix = '/evaluate/sender/'
-    sender_name = "evaluate/sender/sender_form.html"
+    template_name = "evaluate/sender/sender_form.html"
 
     def get(self, request):
-        return super(EvaluateSenderIndex, self).get(request, self.sender_name, self.url_suffix)
+        return super(EvaluateSenderIndex, self).get(request, self.template_name, self.url_suffix)
 
     def post(self, request, **kwargs):
         expected_fields = ['first_name', 'last_name', 'email_address', 'slack_name']
         redirect_url = 'evaluate-sender-list'
-        return super(EvaluateSenderCreate, self).post(request=request, sender_name=self.sender_name,
+        return super(EvaluateSenderCreate, self).post(request=request, template_name=self.template_name,
                                                       url_suffix=self.url_suffix,
                                                       expected_fields=expected_fields,
                                                       redirect_url=redirect_url,
@@ -89,33 +89,33 @@ class EvaluateSenderCreate(ObjectCreate):
 
 class EvaluateControllerIndex(ObjectIndex):
     url_suffix = '/evaluate/controller/'
-    controller_name = "evaluate/controller/controller_index.html"
+    template_name = "evaluate/controller/controller_index.html"
 
     def get(self, request):
-        return super(EvaluateControllerIndex, self).get(request, self.controller_name, self.url_suffix)
+        return super(EvaluateControllerIndex, self).get(request, self.template_name, self.url_suffix)
 
 
 class EvaluateControllerDetail(ObjectDetail):
     url_suffix = '/evaluate/controller/'
-    controller_name = "evaluate/controller/controller_detail.html"
+    template_name = "evaluate/controller/controller_detail.html"
 
     def get(self, request, **kwargs):
-        return super(EvaluateControllerDetail, self).get(request, self.controller_name, self.url_suffix, **kwargs)
+        return super(EvaluateControllerDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
 
 # TODO RELATIONSHIPS WTF HELP
 # class EvaluateControllerCreate(ObjectCreate):
 #     url_suffix = '/evaluate/controller/'
-#     controller_name = "evaluate/controller/controller_form.html"
+#     template_name = "evaluate/controller/controller_form.html"
 #
 #     def get(self, request):
-#         return super(EvaluateControllerIndex, self).get(request, self.controller_name, self.url_suffix)
+#         return super(EvaluateControllerIndex, self).get(request, self.template_name, self.url_suffix)
 #
 #     def post(self, request, **kwargs):
 #         expected_fields = ['scheduled_type', 'scheduled_time', 'expiry_type', 'expiry_time', 'sender', 'template',
 #                            'targets',
 #                            'status']
 #         redirect_url = 'evaluate-controller-list'
-#         return super(EvaluateControllerCreate, self).post(request=request, controller_name=self.controller_name,
+#         return super(EvaluateControllerCreate, self).post(request=request, template_name=self.template_name,
 #                                                           url_suffix=self.url_suffix,
 #                                                           expected_fields=expected_fields,
 #                                                           redirect_url=redirect_url,
@@ -123,31 +123,31 @@ class EvaluateControllerDetail(ObjectDetail):
 
 class EvaluateTargetProfileIndex(ObjectIndex):
     url_suffix = '/evaluate/target/'
-    target_name = "evaluate/target/target_index.html"
+    template_name = "evaluate/target/target_index.html"
 
     def get(self, request):
-        return super(EvaluateTargetProfileIndex, self).get(request, self.target_name, self.url_suffix)
+        return super(EvaluateTargetProfileIndex, self).get(request, self.template_name, self.url_suffix)
 
 
 class EvaluateTargetProfileDetail(ObjectDetail):
     url_suffix = '/evaluate/target/'
-    target_name = "evaluate/target/target_detail.html"
+    template_name = "evaluate/target/target_detail.html"
 
     def get(self, request, **kwargs):
-        return super(EvaluateTargetProfileDetail, self).get(request, self.target_name, self.url_suffix, **kwargs)
+        return super(EvaluateTargetProfileDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
 
 # TODO RELATIONSHIPS WTF HELP
 # class EvaluateTargetProfileCreate(ObjectCreate):
 #     url_suffix = '/evaluate/target/'
-#     target_name = "evaluate/target/target_form.html"
+#     template_name = "evaluate/target/target_form.html"
 #
 #     def get(self, request):
-#         return super(EvaluateTargetProfileIndex, self).get(request, self.target_name, self.url_suffix)
+#         return super(EvaluateTargetProfileIndex, self).get(request, self.template_name, self.url_suffix)
 #
 #     def post(self, request, **kwargs):
 #         expected_fields = ['target_controller', 'target', 'token', 'delivery_status']
 #         redirect_url = 'evaluate-target-list'
-#         return super(EvaluateTargetProfileCreate, self).post(request=request, target_name=self.target_name,
+#         return super(EvaluateTargetProfileCreate, self).post(request=request, template_name=self.template_name,
 #                                                       url_suffix=self.url_suffix,
 #                                                       expected_fields=expected_fields,
 #                                                       redirect_url=redirect_url,
