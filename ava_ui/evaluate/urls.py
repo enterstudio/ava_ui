a0d6ea9826e15f48c9cedd453bd1e7451857915f
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-12-31 19:49:17 +1300

    Sender, Controller and Template basics

--- ava_ui/evaluate/urls.py
+++ ava_ui/evaluate/urls.py
@@ -3,23 +3,14 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^template/email/$', EvaluateTemplateEmailIndex.as_view(),
-        name="evaluate-template-email-index"),
+    url(r'^template/$', EvaluateTemplateIndex.as_view(),
+        name="evaluate-template-index"),
 
-    url(r'^template/email/new/$', EvaluateTemplateEmailCreate.as_view(),
-        name="evaluate-template-email-create"),
+    url(r'^template/new/$', EvaluateTemplateCreate.as_view(),
+        name="evaluate-template-create"),
 
-    url(r'^template/email/(?P<pk>[0-9]+)/$', EvaluateTemplateEmailDetail.as_view(),
-        name="evaluate-template-email-detail"),
-
-    url(r'^template/slack/$', EvaluateTemplateSlackIndex.as_view(),
-        name="evaluate-template-slack-index"),
-
-    url(r'^template/slack/new/$', EvaluateTemplateSlackCreate.as_view(),
-        name="evaluate-template-slack-create"),
-
-    url(r'^template/slack/(?P<pk>[0-9]+)/$', EvaluateTemplateSlackDetail.as_view(),
-        name="evaluate-template-slack-detail"),
+    url(r'^template/(?P<pk>[0-9]+)/$', EvaluateTemplateDetail.as_view(),
+        name="evaluate-template-detail"),
 
     url(r'^results/$', EvaluateResultIndex.as_view(),
         name="evaluate-results-index"),
@@ -27,4 +18,23 @@ urlpatterns = [
     # TODO REMOVED DUE TO TIME CONSTRAINTS - WORKS BUT TEMPLATE.HTML NEEDS LOVE
     # url(r'^results/(?P<pk>[0-9]+)/$', EvaluateResultDetail.as_view(),
     #     name="evaluate-results-detail"),
+    
+    url(r'^sender/$', EvaluateSenderIndex.as_view(),
+        name="evaluate-sender-index"),
+
+    url(r'^sender/new/$', EvaluateSenderCreate.as_view(),
+        name="evaluate-sender-create"),
+
+    url(r'^sender/(?P<pk>[0-9]+)/$', EvaluateSenderDetail.as_view(),
+        name="evaluate-sender-detail"),
+    
+    url(r'^controller/$', EvaluateControllerIndex.as_view(),
+        name="evaluate-controller-index"),
+
+    # TODO relationships during creation
+    # url(r'^controller/new/$', EvaluateControllerCreate.as_view(),
+    #     name="evaluate-controller-create"),
+
+    url(r'^controller/(?P<pk>[0-9]+)/$', EvaluateControllerDetail.as_view(),
+        name="evaluate-controller-detail"),
 ]
--- ava_ui/evaluate/views.py
+++ ava_ui/evaluate/views.py
@@ -5,70 +5,38 @@ from ava_ui.abstract.views import ObjectCreate, ObjectDetail, ObjectIndex
 log = logging.getLogger(__name__)
 
 
-class EvaluateTemplateEmailIndex(ObjectIndex):
-    url_suffix = '/evaluate/template_email/'
-    template_name = "evaluate/template_email/email_template_index.html"
+class EvaluateTemplateIndex(ObjectIndex):
+    url_suffix = '/evaluate/template/'
+    template_name = "evaluate/template/template_index.html"
 
     def get(self, request):
-        return super(EvaluateTemplateEmailIndex, self).get(request, self.template_name, self.url_suffix)
+        return super(EvaluateTemplateIndex, self).get(request, self.template_name, self.url_suffix)
 
 
-class EvaluateTemplateEmailDetail(ObjectDetail):
-    url_suffix = '/evaluate/template_email/'
-    template_name = "evaluate/template_email/email_template_detail.html"
+class EvaluateTemplateDetail(ObjectDetail):
+    url_suffix = '/evaluate/template/'
+    template_name = "evaluate/template/template_detail.html"
 
     def get(self, request, **kwargs):
-        return super(EvaluateTemplateEmailDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+        return super(EvaluateTemplateDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
 
 
-class EvaluateTemplateEmailCreate(ObjectCreate):
-    url_suffix = '/evaluate/template_email/'
-    template_name = "evaluate/template_email/email_template_form.html"
+class EvaluateTemplateCreate(ObjectCreate):
+    url_suffix = '/evaluate/template/'
+    template_name = "evaluate/template/template_form.html"
 
     def get(self, request):
-        return super(EvaluateTemplateEmailIndex, self).get(request, self.template_name, self.url_suffix)
+        return super(EvaluateTemplateIndex, self).get(request, self.template_name, self.url_suffix)
 
     def post(self, request, **kwargs):
-        expected_fields = ['name', 'description', 'subject', 'body']
-        redirect_url = 'evaluate-template-email-list'
-        return super(EvaluateTemplateEmailCreate, self).post(request=request, template_name=self.template_name,
-                                                             url_suffix=self.url_suffix,
-                                                             expected_fields=expected_fields,
-                                                             redirect_url=redirect_url,
-                                                             **kwargs)
+        expected_fields = ['name', 'description', 'template_type', 'email_subject', 'email_body']
+        redirect_url = 'evaluate-template-list'
+        return super(EvaluateTemplateCreate, self).post(request=request, template_name=self.template_name,
+                                                        url_suffix=self.url_suffix,
+                                                        expected_fields=expected_fields,
+                                                        redirect_url=redirect_url,
+                                                        **kwargs)
 
-
-class EvaluateTemplateSlackIndex(ObjectIndex):
-    url_suffix = '/evaluate/template_slack/'
-    template_name = "evaluate/template/slack_template_index.html"
-
-    def get(self, request):
-        return super(EvaluateTemplateSlackIndex, self).get(request, self.template_name, self.url_suffix)
-
-
-class EvaluateTemplateSlackDetail(ObjectDetail):
-    url_suffix = '/evaluate/template_slack/'
-    template_name = "evaluate/template/slack_template_detail.html"
-
-    def get(self, request, **kwargs):
-        return super(EvaluateTemplateSlackDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
-
-
-class EvaluateTemplateSlackCreate(ObjectCreate):
-    url_suffix = '/evaluate/template_slack/'
-    template_name = "evaluate/template_slack/slack_template_form.html"
-
-    def get(self, request):
-        return super(EvaluateTemplateSlackIndex, self).get(request, self.template_name, self.url_suffix)
-
-    def post(self, request, **kwargs):
-        expected_fields = ['name', 'description']
-        redirect_url = 'evaluate-template-slack-list'
-        return super(EvaluateTemplateSlackCreate, self).post(request=request, template_name=self.template_name,
-                                                             url_suffix=self.url_suffix,
-                                                             expected_fields=expected_fields,
-                                                             redirect_url=redirect_url,
-                                                             **kwargs)
 
 class EvaluateResultIndex(ObjectIndex):
     url_suffix = '/evaluate/result/'
@@ -85,3 +53,70 @@ class EvaluateResultIndex(ObjectIndex):
 #
 #     def get(self, request, **kwargs):
 #         return super(EvaluateResultDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+
+class EvaluateSenderIndex(ObjectIndex):
+    url_suffix = '/evaluate/sender/'
+    sender_name = "evaluate/sender/sender_index.html"
+
+    def get(self, request):
+        return super(EvaluateSenderIndex, self).get(request, self.sender_name, self.url_suffix)
+
+
+class EvaluateSenderDetail(ObjectDetail):
+    url_suffix = '/evaluate/sender/'
+    sender_name = "evaluate/sender/sender_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(EvaluateSenderDetail, self).get(request, self.sender_name, self.url_suffix, **kwargs)
+
+
+class EvaluateSenderCreate(ObjectCreate):
+    url_suffix = '/evaluate/sender/'
+    sender_name = "evaluate/sender/sender_form.html"
+
+    def get(self, request):
+        return super(EvaluateSenderIndex, self).get(request, self.sender_name, self.url_suffix)
+
+    def post(self, request, **kwargs):
+        expected_fields = ['first_name', 'last_name', 'email_address', 'slack_name']
+        redirect_url = 'evaluate-sender-list'
+        return super(EvaluateSenderCreate, self).post(request=request, sender_name=self.sender_name,
+                                                      url_suffix=self.url_suffix,
+                                                      expected_fields=expected_fields,
+                                                      redirect_url=redirect_url,
+                                                      **kwargs)
+
+
+class EvaluateControllerIndex(ObjectIndex):
+    url_suffix = '/evaluate/controller/'
+    controller_name = "evaluate/controller/controller_index.html"
+
+    def get(self, request):
+        return super(EvaluateControllerIndex, self).get(request, self.controller_name, self.url_suffix)
+
+
+class EvaluateControllerDetail(ObjectDetail):
+    url_suffix = '/evaluate/controller/'
+    controller_name = "evaluate/controller/controller_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(EvaluateControllerDetail, self).get(request, self.controller_name, self.url_suffix, **kwargs)
+
+# TODO RELATIONSHIPS WTF HELP
+# class EvaluateControllerCreate(ObjectCreate):
+#     url_suffix = '/evaluate/controller/'
+#     controller_name = "evaluate/controller/controller_form.html"
+#
+#     def get(self, request):
+#         return super(EvaluateControllerIndex, self).get(request, self.controller_name, self.url_suffix)
+#
+#     def post(self, request, **kwargs):
+#         expected_fields = ['scheduled_type', 'scheduled_time', 'expiry_type', 'expiry_time', 'sender', 'template',
+#                            'targets',
+#                            'status']
+#         redirect_url = 'evaluate-controller-list'
+#         return super(EvaluateControllerCreate, self).post(request=request, controller_name=self.controller_name,
+#                                                           url_suffix=self.url_suffix,
+#                                                           expected_fields=expected_fields,
+#                                                           redirect_url=redirect_url,
+#                                                           **kwargs)
