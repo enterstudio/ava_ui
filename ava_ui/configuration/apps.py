2f4fb880faed8e627d3ab995d22a6071fe2b8fb6
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-01-02 19:58:41 +1300

    removed old unused app

--- ava_ui/configuration/apps.py
+++
@@ -1,7 +0,0 @@
-# Django imports
-from django.apps import AppConfig
-
-
-# Implementation
-class ConfigurationConfig(AppConfig):
-    name = 'ava_ui.configuration'
--- ava_ui/configuration/views.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-import requests
-from django.shortcuts import render
-from django.views import generic
-
-log = logging.getLogger(__name__)
