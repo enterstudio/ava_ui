8e86a1ac56d99c91cb0189c92257a42bcc08b832
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-01-03 20:24:46 +1300

    Added URLs and views for integrations

--- ava_ui/integration/integration_google/urls.py
+++ ava_ui/integration/integration_google/urls.py
@@ -1,8 +1,18 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 from .views import *
 
-
 urlpatterns = [
-    # url(r'^', ''),
+    url(r'^google/$', GoogleIntegrationIndex.as_view(),
+        name="integration-google-index"),
+
+    url(r'^google/new/$', GoogleIntegrationCreate.as_view(),
+        name="integration-google-create"),
+
+    url(r'^google/(?P<pk>[0-9]+)/$', GoogleIntegrationDetail.as_view(),
+        name="integration-google-detail"),
+
+    url(r'^google/delete/(?P<pk>[0-9]+)/$', GoogleIntegrationDelete.as_view(),
+        name="integration-google-delete"),
+   
 ]
--- ava_ui/integration/integration_google/views.py
+++ ava_ui/integration/integration_google/views.py
@@ -1,7 +1,49 @@
 import logging
 
-import requests
-from django.shortcuts import render
-from django.views import generic
+from ava_ui.abstract.views import ObjectIndex, ObjectDetail, ObjectCreate, ObjectDelete
 
 log = logging.getLogger(__name__)
+
+
+class GoogleIntegrationIndex(ObjectIndex):
+    url_suffix = '/integration/google/setup/'
+    template_name = "integration/google/google_index.html"
+
+    def get(self, request):
+        return super(GoogleIntegrationIndex, self).get(request, self.template_name, self.url_suffix)
+
+
+class GoogleIntegrationDetail(ObjectDetail):
+    url_suffix = '/integration/google/setup/'
+    template_name = "integration/google/google_detail.html"
+
+    def get(self, request, **kwargs):
+        return super(GoogleIntegrationDetail, self).get(request, self.template_name, self.url_suffix, **kwargs)
+
+
+class GoogleIntegrationCreate(ObjectCreate):
+    url_suffix = '/integration/google/setup/'
+    template_name = "integration/google/google_form.html"
+
+    def get(self, request):
+        return super(GoogleIntegrationCreate, self).get(request, self.template_name, self.url_suffix)
+
+    def post(self, request, **kwargs):
+        expected_fields = ['name', 'description', 'domain']
+        redirect_url = 'integration-google-index'
+        return super(GoogleIntegrationCreate, self).post(request=request, template_name=self.template_name,
+                                                   url_suffix=self.url_suffix,
+                                                   expected_fields=expected_fields,
+                                                   redirect_url=redirect_url,
+                                                   **kwargs)
+
+
+class GoogleIntegrationDelete(ObjectDelete):
+    url_suffix = '/integration/google/setup/'
+    redirect_url = 'integration-google-index'
+
+    def get(self, request, **kwargs):
+        return super(GoogleIntegrationDelete, self).get(request=request,
+                                                  url_suffix=self.url_suffix,
+                                                  redirect_url=self.redirect_url,
+                                                  **kwargs)
