0b79c1411d2535558aa63abebb83a415416ac70e
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-01-03 21:51:36 +1300

    Google and LDAP dashboards and imports.

--- ava_ui/gather/gather_google/urls.py
+++ ava_ui/gather/gather_google/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 from .views import *
 
-
 urlpatterns = [
-    # url(r'^', ''),
+    url(r'^google/$', GoogleGatherIndex.as_view(),
+        name="gather-google-index"),
+
+    url(r'^google/(?P<pk>[0-9]+)/$', GoogleGatherImport.as_view(),
+        name="gather-google-import"),
+   
 ]
--- ava_ui/gather/gather_ldap/views.py
+++ ava_ui/gather/gather_ldap/views.py
@@ -1,7 +1,19 @@
 import logging
 
-import requests
-from django.shortcuts import render
-from django.views import generic
+from ava_ui.abstract.views import ObjectDetail, ObjectIndex
 
 log = logging.getLogger(__name__)
+
+class LDAPGatherImport(ObjectDetail):
+    url_suffix = '/gather/ldap/import/'
+    template_name = "gather/ldap/ldap_dashboard.html"
+
+    def get(self, request, **kwargs):
+        return super(LDAPGatherImport, self).get(request, self.template_name, self.url_suffix, **kwargs)
+
+class LDAPGatherIndex(ObjectIndex):
+    url_suffix = '/gather/ldap/'
+    template_name = "gather/ldap/ldap_dashboard.html"
+
+    def get(self, request, **kwargs):
+        return super(LDAPGatherIndex, self).get(request, self.template_name, self.url_suffix, **kwargs)
