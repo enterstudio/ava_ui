61791597a626b6b24e8e4725e394ab9a893e1452
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-01-06 21:59:07 +1300

    Dashboard urls and views

--- ava_ui/dashboards/views.py
+++ ava_ui/dashboards/views.py
@@ -7,7 +7,7 @@ log = logging.getLogger(__name__)
 
 class EducateDashboard(ObjectDashboard):
     url_suffix = ''
-    template_name = "learn/index.html"
+    template_name = "dashboards/educate_index.html"
 
     def get(self, request):
         return super(EducateDashboard, self).get(request, self.template_name, self.url_suffix)
@@ -15,7 +15,7 @@ class EducateDashboard(ObjectDashboard):
 
 class TestDashboard(ObjectDashboard):
     url_suffix = ''
-    template_name = "evaluate/index.html"
+    template_name = "dashboards/test_index.html"
 
     def get(self, request):
         return super(TestDashboard, self).get(request, self.template_name, self.url_suffix)
@@ -23,7 +23,72 @@ class TestDashboard(ObjectDashboard):
 
 class KnowDashboard(ObjectDashboard):
     url_suffix = ''
-    template_name = "organize/index.html"
+    template_name = "dashboards/know_index.html"
 
     def get(self, request):
         return super(KnowDashboard, self).get(request, self.template_name, self.url_suffix)
+
+class MyDashboard(ObjectDashboard):
+    url_suffix = ''
+    template_name = "dashboards/my_index.html"
+
+    def get(self, request):
+        return super(MyDashboard, self).get(request, self.template_name, self.url_suffix)
+
+
+class GatherDashboard(ObjectDashboard):
+    url_suffix = ''
+    template_name = "dashboards/gather_index.html"
+
+    def get(self, request):
+        return super(GatherDashboard, self).get(request, self.template_name, self.url_suffix)
+
+
+class IntegrationDashboard(ObjectDashboard):
+    url_suffix = ''
+    template_name = "dashboards/integration_index.html"
+
+    def get(self, request):
+        return super(IntegrationDashboard, self).get(request, self.template_name, self.url_suffix)
+
+class  GameDashboard(ObjectDashboard):
+    url_suffix = ''
+    template_name = "dashboards/game_index.html"
+
+    def get(self, request):
+        return super(GameDashboard, self).get(request, self.template_name, self.url_suffix)
+
+class EvaluateDashboard(ObjectDashboard):
+    url_suffix = ''
+    template_name = "dashboards/evaluate_index.html"
+
+    def get(self, request):
+        return super(EvaluateDashboard, self).get(request, self.template_name, self.url_suffix)
+
+class LearnDashboard(ObjectDashboard):
+    url_suffix = ''
+    template_name = "dashboards/learn_index.html"
+
+    def get(self, request):
+        return super(LearnDashboard, self).get(request, self.template_name, self.url_suffix)
+
+class OrganizeDashboard(ObjectDashboard):
+    url_suffix = ''
+    template_name = "dashboards/organize_index.html"
+
+    def get(self, request):
+        return super(OrganizeDashboard, self).get(request, self.template_name, self.url_suffix)
+
+class UserDashboard(ObjectDashboard):
+    url_suffix = ''
+    template_name = "dashboards/user_index.html"
+
+    def get(self, request):
+        return super(UserDashboard, self).get(request, self.template_name, self.url_suffix)
+
+class ManagerDashboard(ObjectDashboard):
+    url_suffix = ''
+    template_name = "dashboards/manager_index.html"
+
+    def get(self, request):
+        return super(ManagerDashboard, self).get(request, self.template_name, self.url_suffix)
