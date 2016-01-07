92a4399845f69bf9d02e3426671499b8526b7934
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-01-07 19:16:30 +1300

    Added app config

---
+++ ava_ui/accounts/apps.py
@@ -0,0 +1,10 @@
+# Django Imports
+from django.apps import AppConfig
+
+
+# Implementation
+class MyConfig(AppConfig):
+    name = 'accounts'
+    label = 'accounts'
+    verbose_name = 'accounts'
+
